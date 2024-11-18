a060c46db6e4c2b2a56c742bc10fdf7560e6167c
Samuele Converso <samu.converso@gmail.com>
2024-11-18 19:17:05 +0100

    Commit prima pagina fatta

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,9 @@
 const btnProceed = document.getElementById("btnProceed");
 const btnCheckBox = document.getElementById("checkboxFirstPage");
 const aLink = document.getElementById("aLink");
-let valueCheckBox = btnCheckBox.value;
-
-document.addEventListener("load", init());
+const advice = document.getElementById("advice");
+const adviceText = document.getElementById("advice").innerHTML;
+let valueCheckBox = btnCheckBox.checked;
 
 btnProceed.addEventListener(
   "mouseover",
@@ -14,3 +14,20 @@ btnProceed.addEventListener(
   "mouseleave",
   () => (btnProceed.style.boxShadow = "0px 0px 10px 4px #00ffff")
 );
+
+aLink.onclick = function () {
+  valueCheckBox = btnCheckBox.checked;
+  console.log(valueCheckBox);
+  if (valueCheckBox) {
+    aLink.setAttribute("href", "quiz.html");
+  }
+};
+
+btnCheckBox.onclick = function () {
+  valueCheckBox = btnCheckBox.checked;
+  if (valueCheckBox) {
+    advice.innerHTML = "";
+  } else {
+    advice.innerHTML = adviceText;
+  }
+};
